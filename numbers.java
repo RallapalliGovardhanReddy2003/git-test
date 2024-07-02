bc20856e49d9feee34741d74de4e7a1b57fae9b5
unknown <govardhan.rallapalli@gmail.com>
2024-07-02 23:30:56 +0530

    number java file commiting to git hub

---
+++ numbers.java
@@ -0,0 +1,16 @@
+public class numbers {
+	public static void main(String[] args){
+		{
+		
+		//For start
+		for (int i=0;i<=10;i++) {
+			System.out.println(i);
+		}
+		//For end
+		System.out.println("Block En");
+		}
+	
+	System.out.println("Mehod End");
+	
+	}
+}
